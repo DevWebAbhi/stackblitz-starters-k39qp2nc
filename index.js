edc19393847645af4bf928e1c23ba3d7747e3249
DevWebAbhi <tiwariabhishek889912@gmail.com>
2025-03-07 09:42:37 +0530

    all tasks completed

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ const { resolve } = require('path');
 const app = express();
 const port = 3010;
 
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+// Export the config object
+module.exports = config;
+
+// Log the variables (Optional for testing)
+console.log(config);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
